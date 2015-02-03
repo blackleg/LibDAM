49e5d481e5911b6b3d60e5ef6a9b6c17f4ffbf31
Blackleg <blackleg@openaliasbox.org>
2015-02-03 17:36:44 +0100

    Add StringConNumerosException

---
+++ src/excepciones/StringConNumerosException.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepciones;
+
+/**
+ *
+ * @author hector
+ */
+public class StringConNumerosException extends Exception {
+
+    /**
+     * Creates a new instance of <code>StringConNumerosException</code> without
+     * detail message.
+     */
+    public StringConNumerosException() {
+    }
+
+    /**
+     * Constructs an instance of <code>StringConNumerosException</code> with the
+     * specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public StringConNumerosException(String msg) {
+        super(msg);
+    }
+}
