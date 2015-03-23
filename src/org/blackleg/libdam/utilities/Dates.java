baf017237c587b66581f26bfbdf54a563ee63348
hespert <hespert@indra.es>
2015-03-23 16:19:43 +0100

    Add New methods in Dates

--- src/org/blackleg/libdam/utilities/Dates.java
+++ src/org/blackleg/libdam/utilities/Dates.java
@@ -7,7 +7,9 @@ package org.blackleg.libdam.utilities;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  *
@@ -18,6 +20,30 @@ public class Dates {
     public static Date fromStringWithFormat(String format, String stringFecha) throws ParseException {
         SimpleDateFormat formatoDelTexto = new SimpleDateFormat(format);
         return formatoDelTexto.parse(stringFecha);
+    }
+    
+    public static Integer differenceInMonths(Date beginningDate, Date endingDate) {
+        if (beginningDate == null || endingDate == null) {
+            return 0;
+        }
+        Calendar cal1 = new GregorianCalendar();
+        cal1.setTime(beginningDate);
+        Calendar cal2 = new GregorianCalendar();
+        cal2.setTime(endingDate);
+        return differenceInMonths(cal1, cal2);
+    }
+
+    public static Integer differenceInMonths(Calendar beginningDate, Calendar endingDate) {
+        if (beginningDate == null || endingDate == null) {
+            return 0;
+        }
+        int m1 = beginningDate.get(Calendar.YEAR) * 12 + beginningDate.get(Calendar.MONTH);
+        int m2 = endingDate.get(Calendar.YEAR) * 12 + endingDate.get(Calendar.MONTH);
+        return m2 - m1;
+    }
+    
+    public static Integer transformMonthsCountToYears(Integer months) {
+        return months / 12;
     }
     
 }
